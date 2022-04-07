adfa001b98ecccec525331e6d21ce4f02ce3c0ac
Vitaly <lub-olen@yandex.ru>
2022-04-08 02:35:34 +0300

    Initial Commit

---
+++ Controls Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Controls Practice
+//
+//  Created by Vitally Ochnev on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
